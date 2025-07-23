dd31d4ee92af7f426ae43b3a4016cc760d4753e2
Guillaume Sere <guillaumesere@hotmail.fr>
2025-07-23 11:10:20 +0200

    modif css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="container">
         <Header />
         <main>{children}</main>
       </body>
