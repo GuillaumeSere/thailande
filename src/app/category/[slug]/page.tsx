5c7666bad2067897a4d59ce29837796c631749d0
Guillaume Sere <guillaumesere@hotmail.fr>
2025-07-23 11:25:16 +0200

    modif

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -1,23 +1,21 @@
-import data from '../../../data/media_updated.json';
+// src/app/category/[slug]/page.tsx
+
+import data from '@/data/media_updated.json';
 import { notFound } from 'next/navigation';
-import type { Category } from '../../../lib/types';
-import CategoryPageClient from '../../../components/CategoryPageClient';
+import type { Category } from '@/lib/types';
+import CategoryPageClient from '@/components/CategoryPageClient';
 
-type CategoryPageProps = {
-  params: {
-    slug: string;
-  };
-};
-
-const CategoryPage = ({ params }: CategoryPageProps) => {
-  const { slug } = params;
-
+export default async function CategoryPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
   const createSlug = (name: string) => {
     return name.toLowerCase().replace(/\s+/g, '-');
   };
 
   const category = (data.categories as Category[]).find(
-    (c: Category) => createSlug(c.name) === slug
+    (c) => createSlug(c.name) === params.slug
   );
 
   if (!category) {
@@ -25,18 +23,16 @@ const CategoryPage = ({ params }: CategoryPageProps) => {
   }
 
   return <CategoryPageClient category={category} />;
-};
-
-export default CategoryPage;
+}
 
 export async function generateStaticParams() {
   const createSlug = (name: string) => {
     return name.toLowerCase().replace(/\s+/g, '-');
   };
 
-  return (data.categories as Category[]).map((category: Category) => ({
-   params: {
-    slug: createSlug(category.name),
-   }
+  return (data.categories as Category[]).map((category) => ({
+    params: {
+      slug: createSlug(category.name),
+    },
   }));
-} 
+}
